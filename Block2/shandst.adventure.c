4f4e89545b26a355876cf9a46fd9bf7f1d8a5dbd
Travis Shands <shandstp@gmail.com>
2019-10-27 12:33:10 -0700

    Created initial Block2 directory

---
+++ Block2/shandst.adventure.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main()
+{
+    return 0;
+}
---
+++ Block2/shandst.buildrooms.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main(){
+
+    return 0;
+}
