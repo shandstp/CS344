d2e44bc600c0f32c7426d2170a9a4d8714120f83
Travis Shands <shandstp@gmail.com>
2019-10-31 13:27:14 -0700

    Finished buildrooms program; adventure program succesfully selects most recent folder for operations

--- Block2/shandst.adventure.c
+++ Block2/shandst.adventure.c
@@ -4,8 +4,48 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <stdlib.h>
+
+struct Files{
+	int numEntries;
+	char** enties;
+};
+
+char* selectDir(){
+
+	int mostRecent = -1;
+	char dirPrefix[32] = "shandst.rooms.";
+	int nameLen = 256;
+	char* newDir = malloc(nameLen * sizeof(char));
+	memset(newDir, '\0', nameLen);
+
+	DIR* topDir;
+	struct dirent *curDir;
+	struct stat dirInfo;
+
+	topDir = opendir(".");
+	if(topDir > 0){
+		while((curDir = readdir(topDir)) != NULL){
+			if(strstr(curDir->d_name, dirPrefix) != NULL){
+				stat(curDir->d_name, &dirInfo);
+				
+				if((int)dirInfo.st_mtime > mostRecent){
+					mostRecent = (int)dirInfo.st_mtime;
+					memset(newDir, '\0', nameLen);
+					strcpy(newDir, curDir->d_name);
+				}
+			}
+		}
+	}
+
+
+	closedir(topDir);
+	return newDir;
+}
 
 int main()
 {
-    return 0;
+	char* dir = selectDir();
+	printf("The most recent dir is: %s\n", dir);
+	return 0;
 }
--- Block2/shandst.buildrooms.c
+++ Block2/shandst.buildrooms.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
 #include <string.h>
 #include <dirent.h>
 #include <sys/types.h>
@@ -6,178 +10,257 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdlib.h>
-#include <time.h>
 
-struct room{
+
+
+enum truth {false, true};
+
+struct Room {
 	int id;
+	char* type;
 	int numConnect;
-	char* roomName;
-	struct room* connection[6];
+	char* rmName;
+	struct Room* outbound[6];
 };
 
+struct Dungeon {
+	int capacity;
+	int numRooms;
+	struct Room* dungeonMap[7];
+};
 
-int isGraphFull(struct room* dungeon){
+void initRoom(struct Room* room){
+	room->numConnect = 0;
+	room->type = calloc(16, sizeof(char));
+	room->rmName = calloc(16, sizeof(char));
 	int i;
-	for(i = 0; i < 7; i++){
-		if(dungeon[i].numConnect < 3){
+	for(i = 0; i < 6; i ++){
+		room->outbound[i] = malloc(sizeof(struct Room));
+	}
+}
+
+void initDungeon(struct Dungeon* d){
+	d->capacity = 7;
+	d->numRooms = 0;
+	int i;
+	for(i = 0; i < d->capacity; i++){
+        d->dungeonMap[i] = malloc(sizeof(struct Room));
+		initRoom(d->dungeonMap[i]);
+	}
+}
+
+void printDungeon(struct Dungeon* d){
+	printf("Details of the dungeon are as follows:\n");
+	printf("Room capacity: %d\n", d->capacity);
+	printf("Number of rooms assigned: %d\n", d->numRooms);
+	int i;
+	for(i = 0; i < d->capacity; i++){
+		printf("Room %d is at: %p\n", i, d->dungeonMap[i]);
+		if(d->dungeonMap[i] != NULL){
+			printf("Name: %s\n", d->dungeonMap[i]->rmName);
+		}
+	}
+}
+
+int containsRoom(struct Dungeon* d, int id){
+    int i;
+    for(i = 0; i < d->capacity; i++){
+        if(d->dungeonMap[i]->id == id){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void getName(struct Dungeon* d, int id){
+    switch (id)
+    {
+        case 1:
+            d->dungeonMap[d->numRooms]->id = id;
+            strcpy(d->dungeonMap[d->numRooms]->rmName, "Crypt");
+            break;
+        case 2:
+            d->dungeonMap[d->numRooms]->id = id;
+            strcpy(d->dungeonMap[d->numRooms]->rmName, "Cellar");
+            break;
+        case 3:
+            d->dungeonMap[d->numRooms]->id = id;
+            strcpy(d->dungeonMap[d->numRooms]->rmName, "Library");
+            break;
+        case 4:
+            d->dungeonMap[d->numRooms]->id = id;
+            strcpy(d->dungeonMap[d->numRooms]->rmName, "Colosseum");
+            break;
+        case 5:
+            d->dungeonMap[d->numRooms]->id = id;
+            strcpy(d->dungeonMap[d->numRooms]->rmName, "Wall");
+            break;
+        case 6:
+            d->dungeonMap[d->numRooms]->id = id;
+            strcpy(d->dungeonMap[d->numRooms]->rmName, "Hall");
+            break;
+        case 7:
+            d->dungeonMap[d->numRooms]->id = id;
+            strcpy(d->dungeonMap[d->numRooms]->rmName, "Battlement");
+            break;
+        case 8:
+            d->dungeonMap[d->numRooms]->id = id;
+            strcpy(d->dungeonMap[d->numRooms]->rmName, "Hole");
+            break;
+        case 9:
+            d->dungeonMap[d->numRooms]->id = id;
+            strcpy(d->dungeonMap[d->numRooms]->rmName, "Attic");
+            break;
+        case 10:
+            d->dungeonMap[d->numRooms]->id = id;
+            strcpy(d->dungeonMap[d->numRooms]->rmName, "Garden");
+            break;
+    }
+}
+
+void getType(struct Dungeon* d){
+	switch(d->numRooms)
+	{
+		case 0:
+			strcpy(d->dungeonMap[d->numRooms]->type, "START_ROOM");
+			break;
+		case 6:
+			strcpy(d->dungeonMap[d->numRooms]->type, "END_ROOM");
+			break;
+		default:
+			strcpy(d->dungeonMap[d->numRooms]->type, "MID_ROOM");
+			break;
+	}
+}
+
+void chooseRooms(struct Dungeon* d){
+    while(d->numRooms < d->capacity){
+        int id = (rand() % 10) + 1;
+        if(containsRoom(d, id) != 1){
+            getName(d, id);
+	    getType(d);
+            d->numRooms++;
+        }
+    }
+}
+
+int fullyConnected(struct Dungeon* d){
+	int i;
+	for(i = 0; i < d->capacity; i++){
+		if(d->dungeonMap[i]->numConnect < 3){
 			return 0;
 		}
 	}
 	return 1;
 }
 
-int getRand(int bound){
-	srand(time(0));
-	return rand() % bound;
-}
-
-int canAddConnectionFrom(struct room* dungeon, int a){
-	if(dungeon[a].numConnect < 6){
+int hasRoom(struct Room* r){
+	if(r->numConnect < 6){
 		return 1;
-	}
-	else{
-		return 0;
-	}	
-}
-
-int connectionAlreadyExists(struct room* dungeon, int a, int b){
-	int i;
-	for(i = 0; i < dungeon[a].numConnect; i++){
-		if(dungeon[a].connection[i] == &dungeon[b]){
-			return 1;
-		}
 	}
 	return 0;
 }
 
-void connectRoom(struct room* dungeon, int a, int b){
-	dungeon[a].connection[dungeon[a].numConnect] = &dungeon[b];
-	dungeon[b].connection[dungeon[b].numConnect] = &dungeon[a];
-	dungeon[a].numConnect++;
-	dungeon[b].numConnect++;
-}
-
-int isSameRoom(struct room* dungeon, int a, int b){
-	if(dungeon[a].id == dungeon[b].id){
-		return 1;
-	}
-	else{
-		return 0;
-	}
-}
-
-void addRandomConnection(struct room* dungeon){
-	//struct room a;
-	//struct room b;
-	int a;
-	int b;
-
-	while(1){
-		a = getRand(7);
-		if(canAddConnectionFrom(dungeon, a) == 1) break;
-	}
-
-	do{
-		b = getRand(7);
-
-	}
-	while(canAddConnectionFrom(dungeon, b) == 0 || isSameRoom(dungeon, a, b) == 1 || connectionAlreadyExists(dungeon, a, b) == 1);
-
-	connectRoom(dungeon, a, b);
-	connectRoom(dungeon, b, a);
-}
-
-void buildGraphs(struct room* dungeon){
-	while(isGraphFull(dungeon) == 0){
-		addRandomConnection(dungeon);
-	}
-}
-
-int* chooseRooms(){
-	int static roomNums [7];
-	int numChosen = 0;
-	while(numChosen < 7){
-		int numChoice = getRand(10);
-		int used = 0;
-		int i;
-		for(i = 0; i < numChosen; i++){
-			if(roomNums[i] == numChoice){
-				used = 1;
-			}
-		}
-		if(used == 0){
-			roomNums[numChosen] = numChoice;
-			numChosen++;
-		}
-	}
-	return roomNums;
-}
-
-void makeDungeon(struct room* dungeon){
-	int* roomNums = chooseRooms();
+int alreadyAdded(struct Dungeon* d, int x, int y){
 	int i;
-	for(i = 0; i < 7; i++){
-		dungeon[i].numConnect = 0;
-		switch(roomNums[i]){
-			case 0:
-				dungeon[i].id = i;
-				dungeon[i].roomName = "Arboretum";	
-				break;
-			case 1:
-				dungeon[i].id = i;
-				dungeon[i].roomName = "Library";
-				break;
-			case 2:	
-				dungeon[i].id = i;
-				dungeon[i].roomName = "Barracks";
-				break;
-			case 3:	
-				dungeon[i].id = i;
-				dungeon[i].roomName = "The Pit";
-				break;
-			case 4:	
-				dungeon[i].id = i;
-				dungeon[i].roomName = "The Lab";
-				break;
-			case 5:	
-				dungeon[i].id = i;
-				dungeon[i].roomName = "Courtyard";
-				break;
-			case 6:	
-				dungeon[i].id = i;
-				dungeon[i].roomName = "The Hall";
-				break;
-			case 7:	
-				dungeon[i].id = i;
-				dungeon[i].roomName = "Scriptorium";
-				break;
-			case 8:	
-				dungeon[i].id = i;
-				dungeon[i].roomName = "The Gallows";
-				break;
-			case 9:	
-				dungeon[i].id = i;
-				dungeon[i].roomName = "Scrying Chamber";
-				break;
+	for(i = 0; i < d->dungeonMap[x]->numConnect; i++){
+		if(d->dungeonMap[y]->id == d->dungeonMap[x]->outbound[i]->id){
+			return 1;
 		}
+	}	
+	return 0;
+}
+
+void connectRooms(struct Room* r1, struct Room* r2){
+	r1->outbound[r1->numConnect] = r2;
+	r1->numConnect++;
+	r2->outbound[r2->numConnect] = r1;
+	r2->numConnect++;	
+}
+
+void connectDungeon(struct Dungeon* d){
+	while(fullyConnected(d) != true){
+		int x = rand() % d->capacity;	
+		int y = rand() % d->capacity;
+		while(hasRoom(d->dungeonMap[x]) == false){
+			x = rand() % d->capacity; 	
+		}
+		do{
+			y = rand() % d->capacity;
+		} while(x == y || hasRoom(d->dungeonMap[y]) == false || alreadyAdded(d, x, y) == true);
+		
+		connectRooms(d->dungeonMap[x], d->dungeonMap[y]);	
 	}
-} 
+}
 
-int main(int argc, char *argv[]){
+void printRooms(struct Dungeon* d){
+   int i;
+   for(i = 0; i < 7; i++){
+   	printf("%s is a %s and is connected to:\n", d->dungeonMap[i]->type, d->dungeonMap[i]->rmName); 
+       	int j;
+       	for(j = 0; j < 6; j++){
+            printf("%s ", d->dungeonMap[i]->outbound[j]->rmName);
+       
+        }
+        printf("\n");
+    }
+}
 
-	struct room dungeon[7];
-	makeDungeon(dungeon);	
-	buildGraphs(dungeon);
+char* newDir(){
+    int procid = getpid();
+    char dirPrfx [32] = "shandst.rooms.";
+    char* dirName = calloc(32, sizeof(char));
+    sprintf(dirName, "%s%d", dirPrfx, procid);
+    mkdir(dirName, 0776);
+
+    return dirName;
+}
+
+int main(){
+
+    	srand(time(0));
+
+	struct Dungeon* dungeon = malloc(sizeof(struct Dungeon));
+	initDungeon(dungeon);
+    	chooseRooms(dungeon);
+	connectDungeon(dungeon);
+
+//	printDungeon(dungeon);
+//   	printRooms(dungeon);
+
+	char* curDir = newDir();
+//	printf("Created %s\n", curDir);
 	int i;
-	printf("The rooms map out as follows:\n");
-	for(i = 0; i < 7; i++){
-		printf("%s is connected to ", dungeon[i].roomName);
+	for(i = 0; i < dungeon->numRooms; i++){
+		char path[32] = {'\0'};
+		sprintf(path, "%s/%s", curDir, dungeon->dungeonMap[i]->rmName);
+		int fd = open(path, O_WRONLY | O_APPEND | O_CREAT, 0644);
+		if(fd == -1){
+			printf("Failed to open file\n");
+			perror("While opening file");
+			exit(1);
+		}
+	
+//		printf("Created file with descriptor %d, in %s\n", fd, path);
+//		ssize_t writeCount;
+		char* output = calloc(32, sizeof(char));
+		memset(output, '\0', 32);
+		sprintf(output, "ROOM NAME: %s\n", dungeon->dungeonMap[i]->rmName);
+//		writeCount = write(fd, output, strlen(output) * sizeof(char));
+		write(fd, output, strlen(output) * sizeof(char));
 		int j;
-		for(j = 0; j < dungeon[i].numConnect; j++){
-			printf("%s ", dungeon[i].connection[j]->roomName);	
+		for(j = 0; j < dungeon->dungeonMap[i]->numConnect; j++){
+			memset(output, '\0', 32);
+			sprintf(output, "CONNECTION %d: %s\n", j + 1, dungeon->dungeonMap[i]->outbound[j]->rmName);
+//			writeCount = write(fd, output, strlen(output) * sizeof(char));
+			write(fd, output, strlen(output) * sizeof(char));
+//			printf("Wrote %zu bytes to %s\n", writeCount, path);
 		}
-		printf("\n");
+		memset(output, '\0', 32);
+		sprintf(output, "ROOM TYPE: %s\n", dungeon->dungeonMap[i]->type);
+//		writeCount = write(fd, output, strlen(output) * sizeof(char));
+		write(fd, output, strlen(output) * sizeof(char));
 	}
-	printf("\n");
+ 
 	return 0;
 }
