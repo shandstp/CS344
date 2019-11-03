1f07435fcd3fa7c7787846d9d4d476e7ac86aeca
Travis Shands <shandstp@gmail.com>
2019-11-02 20:51:31 -0700

    Finished

--- Block2/shandst.adventure.c
+++ Block2/shandst.adventure.c
@@ -1,3 +1,5 @@
+#define _GNU_SOURCE //Silences warning about implicit declaration of pthread_tryjoin_np
+
 #include <stdio.h>
 #include <string.h>
 #include <dirent.h>
@@ -9,35 +11,39 @@
 #include <time.h>
 #include <pthread.h>
 
+pthread_mutex_t myMute = PTHREAD_MUTEX_INITIALIZER; //Initialize mutex
+
+//Used to keep track of how many steps the user took, which rooms they visited, and in what order
 struct gameState{
-	int steps;
-	char** visited;
+	int steps; //stores a count of how many steps a user took toward victory
+	char** visited; //stores the name of each room visited during game play in the order in which the rooms were visited
 };
 
 struct roomFile{
-	int numEntries;
-	char** entries;
+	int numEntries; //Stores a count of how many entries were found in room's room file
+	char** entries; //Stores name of current room at entries[0], the room's type at entries[numEntries], and names of connecting rooms at every index in between
 };
 
+//Opens ".", checks each directory that begins with "shandst.rooms." to find the most recently created, and returns its name
 char* selectDir(){
 
-	int mostRecent = -1;
-	char dirPrefix[32] = "shandst.rooms.";
+	int mostRecent = -1; //prepare to store timestamp of most recent directory
+	char dirPrefix[32] = "shandst.rooms."; //Only directories starting with this string should be checked
 	int nameLen = 256;
-	char* newDir = malloc(nameLen * sizeof(char));
+	char* newDir = malloc(nameLen * sizeof(char)); //Prepare to store name of most recently created directory
 	memset(newDir, '\0', nameLen);
 
 	DIR* topDir;
 	struct dirent *curDir;
 	struct stat dirInfo;
 
-	topDir = opendir(".");
+	topDir = opendir("."); //Open the present working directory
 	if(topDir > 0){
-		while((curDir = readdir(topDir)) != NULL){
-			if(strstr(curDir->d_name, dirPrefix) != NULL){
+		while((curDir = readdir(topDir)) != NULL){ //Read each entry in the directory
+			if(strstr(curDir->d_name, dirPrefix) != NULL){ //If currently examined directory contains substring "shands.rooms." get its timestamp
 				stat(curDir->d_name, &dirInfo);
 				
-				if((int)dirInfo.st_mtime > mostRecent){
+				if((int)dirInfo.st_mtime > mostRecent){ //Compare current timestamp to current mostRecent. Replace if more recent
 					mostRecent = (int)dirInfo.st_mtime;
 					memset(newDir, '\0', nameLen);
 					strcpy(newDir, curDir->d_name);
@@ -48,144 +54,193 @@ char* selectDir(){
 
 
 	closedir(topDir);
-	return newDir;
+	return newDir; //Return name of most recently created directory
 }
 
+//Sets roomFile to default values
 void initFile(struct roomFile* rf){
-	rf->numEntries = 0;
-	rf->entries = malloc(8 * sizeof(char*));
+	rf->numEntries = 0; //Sets numEntries to 0 so that it can be used to count how many values have been read in from the current room file
+	rf->entries = malloc(8 * sizeof(char*)); //Initialize entries to store array of strings 
 	int i;
 	for(i = 0; i < 8; i++){
-		rf->entries[i] = malloc(256 * sizeof(char));
+		rf->entries[i] = malloc(256 * sizeof(char)); //Initialize entries array for storing values read in from file
 	}
 }
 
+//Sets gameState to default values
 void initGame(struct gameState* gs){
-	gs->steps = 0;
-	gs->visited = malloc(32 * sizeof(char*));
+	gs->steps = 0; //Set step count to 0 so that it can be used to count the number of steps taken by the player toward victory
+	gs->visited = malloc(32 * sizeof(char*)); //Initialize visited to store array of strings
 	int i;
 	for(i = 0; i < 32; i++){
-		gs->visited[i] = malloc(256 * sizeof(char));
+		gs->visited[i] = malloc(256 * sizeof(char)); //Initialize visited array for storing names of rooms visited during gameplay
 	}
 }
 
+//Takes file descriptor and uses it to read contents of file into a roomFile struct
 void getData(struct roomFile* rf, int fd){
 	char buff[256];
 	char word[32];
 	char* mode = malloc(sizeof(char));
-	*mode = 'r';
-	FILE* stream = fdopen(fd, mode);
-	while(fgets(buff, 256, stream) != NULL){
-		memset(word, '\0', 32);
-		sscanf(buff, "%*s %*s %s", word);
-		strcpy(rf->entries[rf->numEntries], word);
-		rf->numEntries++;
+	*mode = 'r'; //Sets fdopen operation mode to read only
+	FILE* stream = fdopen(fd, mode); //Open room file as stream for reading
+	while(fgets(buff, 256, stream) != NULL){ //While there are line remaining, get one line at a time from room file
+		memset(word, '\0', 32); //Reset word each iteration to prepare it to store key data from each line
+		sscanf(buff, "%*s %*s %s", word); //Select the third space seperated string from each line and store it in word
+		strcpy(rf->entries[rf->numEntries], word); //Place key data from each line into most recent room file entry
+		rf->numEntries++; //Increment to keep track of how many entries have been read in
 		memset(buff, '\0', 256);
 	}
 	free(mode);
 }
 
+//Reset roomFile struct to facilitate reading data from next file
 void reset(struct roomFile* rf){
 	int i;
-	for(i = 0; i < rf->numEntries; i++){
+	for(i = 0; i < rf->numEntries; i++){ //Use numEntries to determine how many entries need to be reset to '\0'
 		memset(rf->entries[i], '\0', 256);
 	} 
-	rf->numEntries = 0;
+	rf->numEntries = 0; //Reset count, now that it is no longer needed
 }
 
+//Iterates through each entry in a given directory opening and reading files into a roomFile struct. After reading a file,
+//if opt == 0, compares room name to str, if opt == 1, compares room type to str. In either case, if the strings match, 
+//the roomFile struct is returned with the matching room data
 struct roomFile* findRoom(struct roomFile* rf, DIR* dir, char* path, char* str, int opt){
-	rewinddir(dir);
-	struct dirent* curFile;
+	rewinddir(dir); //Start search files from the top of the directory each time
+	struct dirent* curFile; 
 	char* filePath = malloc(256 * sizeof(char));
-	while((curFile = readdir(dir)) != NULL){
-		if(strcmp(curFile->d_name, ".") != 0 && strcmp(curFile->d_name, "..") != 0){	
+	while((curFile = readdir(dir)) != NULL){ //Loop through each item in directory
+		if(strcmp(curFile->d_name, ".") != 0 && strcmp(curFile->d_name, "..") != 0){ //Do not examine "." or ".."	
 			memset(filePath, '\0', 256 * sizeof(char));	
-			sprintf(filePath, "%s/%s", path, curFile->d_name);
-			int fd = open(filePath, O_RDONLY);
+			sprintf(filePath, "%s/%s", path, curFile->d_name); //Assemble path to file for opening 
+			int fd = open(filePath, O_RDONLY); //Open room file for reading
 			if(fd == -1){
 				printf("Failed to open file in game directory\n");
 				perror("While opening file");
 				exit(1);
 			}
-			getData(rf, fd);
+			getData(rf, fd); //Read data from room file into roomFile struct
 			if(opt == 0){
-				if(strcmp(str, rf->entries[0]) == 0){
+				if(strcmp(str, rf->entries[0]) == 0){ //If option 0 was specified, check if the current file's name matches a given string
 					return rf;
 				}
 			}
 			else if(opt == 1){
-				if(strcmp(str, rf->entries[rf->numEntries - 1]) == 0){
+				if(strcmp(str, rf->entries[rf->numEntries - 1]) == 0){ //If option 1 was specified, check if the current file's type matches a given string
 					return rf;
 				}
 			}
-			reset(rf);
+			reset(rf); //Reset roomFile struct so that it can be used for reading in data from the next file if not match has been found yet
 		}
 	} 
 	return NULL;
 }
 
+//Takes a roomfile struct and presents the data it contains in a formatted prompt to the player
 void prompt(struct roomFile* rf){
-	printf("CURRENT LOCATION: %s\n", rf->entries[0]);
+	printf("CURRENT LOCATION: %s\n", rf->entries[0]); //Presents the name of the current room to the player
 	printf("POSSIBLE CONNECTIONS: ");
 	int i;
-	for(i = 1; i < rf->numEntries - 3; i++){
+	for(i = 1; i < rf->numEntries - 3; i++){ //Loops through all but the last of the current room's connections and lists them seperated by a comma and space 
 		printf("%s, ", rf->entries[i]);
 	}
-	printf("%s.\n", rf->entries[rf->numEntries -2]);
-	printf("WHERE TO? >");
+	printf("%s.\n", rf->entries[rf->numEntries -2]); //Prints the last of the current room's connections, followed by a period and newline
+	printf("WHERE TO? >"); //Presents prompt to the player
 }
 
+//Compares input from user to each of the current room's connections to determine if the user entered a valid option
 int validateRoom(struct roomFile* rf, char* rmName){
 	int i;
 	for(i = 1; i < rf->numEntries - 1; i++){
-		if(strcmp(rf->entries[i], rmName) == 0){
-			return 1;
+		if(strcmp(rf->entries[i], rmName) == 0){ 
+			return 1; //Returns 1 if the player entered a valid room name
 		}
 	}
-	return 0;
+	return 0; //Returns 0 if the player entered an invalid room name
 }
 
+//Prints victory message and gameplay details
 void victory(struct gameState* g, struct roomFile* rf){
-	printf("YOU HAVE FOUND THE END ROOM. CONGRADULATIONS!\nYOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", g->steps);
+	printf("\nYOU HAVE FOUND THE END ROOM. CONGRADULATIONS!\nYOU TOOK %d STEPS. YOUR PATH TO VICTORY WAS:\n", g->steps);
 	int i;
 	for(i = 0; i < g->steps; i++){
-		printf("%s\n", g->visited[i]);
+		printf("%s\n", g->visited[i]); //Lists the rooms that were visited during gameplay, in the order that they were visited
 	}
+}
+
+//Locks the myMute mutex, gets the current date/time, formats it, and writes it to time.txt, as soon as myMute is unlocked 
+void* getTime(void *arg){
+	pthread_mutex_lock(&myMute); //Locks myMute, as soon as it is unlocked by the main thread
+	char* root = (char*) arg;
+	char* path = malloc(256 * sizeof(char));
+	sprintf(path, "%s/time.txt", root); //Assemble path to time.txt file
+	char curTime[64] = {'\0'};
+	
+	time_t t;
+	t = time(NULL); //Get current time
+	struct tm* bdt = localtime(&t); //Convert raw time into broken-down time
+	strftime(curTime, 64, "%I:%M%P, %A, %B %d %Y %n", bdt); //Takes broken-down time and formats it for output
+	int fd = open(path, O_WRONLY | O_TRUNC | O_CREAT, 0644); //Open time.txt for writing. If it doesn't exist, create it, if it does, overwrite it
+	write(fd, curTime, strlen(curTime) * sizeof(char)); //Write formatted time to time.txt
+	close(fd); //Close time.txt
+	pthread_mutex_unlock(&myMute); //Unlock mutex to yield control back to main
+	return NULL;
 }
 
 int main()
 {
+	pthread_t thread; 
+	pthread_mutex_lock(&myMute); //Lock mutex to prevent getTime from running
+	
 	char* dir = selectDir();
 	struct gameState* game = malloc(sizeof(struct gameState));
-	initGame(game);
+	initGame(game); //Initialize game state 
 	struct roomFile* roomFile = malloc(sizeof(struct roomFile));
-	initFile(roomFile);
+	initFile(roomFile); //Initialize roomFile struct
 	char dirPath[256] = {'\0'};
-	sprintf(dirPath, "./%s", dir);
+	sprintf(dirPath, "./%s", dir); //Compose path to game directory
+	pthread_create(&thread, NULL, getTime, dirPath); //Create thread for getting time
+	pthread_tryjoin_np(thread, NULL); //Attempt to join thread without blocking
 	DIR* gameDir = opendir(dirPath); 	
-	roomFile = findRoom(roomFile, gameDir, dirPath, "START_ROOM", 1);
+	roomFile = findRoom(roomFile, gameDir, dirPath, "START_ROOM", 1); //Find start room file and load data into roomFile struct
 	if(roomFile == NULL){
 		printf("Could not locate room\n");
 		exit(1);
 	}
 	size_t buffSize = 0;
 	char* userInput = NULL;
-	while(strcmp(roomFile->entries[roomFile->numEntries - 1], "END_ROOM") != 0){
-		prompt(roomFile);
-		getline(&userInput, &buffSize, stdin);
+	while(strcmp(roomFile->entries[roomFile->numEntries - 1], "END_ROOM") != 0){ //Continue game until room file with "END_ROOM" type is found
+		prompt(roomFile); //Present player with prompt
+		getline(&userInput, &buffSize, stdin); //Get user input
 		int len = strlen(userInput);
-		userInput[len - 1] = '\0';
-		if(validateRoom(roomFile, userInput) == 1){
-			strcpy(game->visited[game->steps], userInput);
-			game->steps++;
-			initFile(roomFile);
-			roomFile = findRoom(roomFile, gameDir, dirPath, userInput, 0);
+		userInput[len - 1] = '\0'; //Replace '\n' in user input with '\0'
+		if(strcmp(userInput, "time") == 0){ //Determines if the player has entered "time" at the prompt
+			pthread_mutex_unlock(&myMute); //Unlocks mutex when player enters "time" at prompt to allow time getting thread to operate
+			usleep(1000); //Pauses long enough to allow time getting thread to lock mutex, but not long enough for player to notice a delay
+			pthread_mutex_lock(&myMute); //Locks the mutex again once the time getting thread unlocks it
+			char timePath[256] = {0};
+			sprintf(timePath, "%s/time.txt", dirPath); //Compose path to time.txt
+			char* mode = malloc(sizeof(char) * 10);
+			*mode = 'r'; //Set mode for opening file stream to read only
+			int fd = open(timePath, O_RDONLY); //open time.txt
+			FILE* stream = fdopen(fd, mode); //Load time.txt into a file stream
+			char word[64] = {'\0'};
+			fgets(word, 64, stream); //Get the first line of time.txt and store it in word
+			printf("\n %s\n", word); //Print the formatted time data to console
+			close(fd); //close time.txt
+		}
+		else if(validateRoom(roomFile, userInput) == 1){ //Determine if the player entered the name of a valid connecting room
+			strcpy(game->visited[game->steps], userInput); //Record valid connecting room choice as a visited room
+			game->steps++; //Increment the number of steps the user has taken toward victory
+			reset(roomFile); //Prepare roomfile struct to store data for the connecting room that the player chose
+			roomFile = findRoom(roomFile, gameDir, dirPath, userInput, 0); //Gets data for new current room
 		}
 		else{
-			printf("\nHUH? I DON'T UNDERSTAND THAT ROOM. TRY AGAIN.\n\n");
+			printf("\nHUH? I DON'T UNDERSTAND THAT ROOM. TRY AGAIN.\n\n"); //Presents player with error message if invalid choice was entered
 		}	
 	}
-	victory(game, roomFile);
+	victory(game, roomFile); //Declares victory if "END_ROOM" is reached
 	closedir(gameDir);
 	return 0;
 }
--- Block2/shandst.buildrooms.c
+++ Block2/shandst.buildrooms.c
@@ -33,10 +33,12 @@ struct Dungeon {
 
 //Takes a Room struct and initializes it to the default values of 0 connections and no name or type values
 void initRoom(struct Room* room){
-	room->numConnect = 0;
+	room->numConnect = 0; //Sets a room struct's connection count to 0 so that room->numConnect can be used to track how many connections each room has
+	//Prepare room->type and room->rmName for storing strings
 	room->type = calloc(16, sizeof(char));
 	room->rmName = calloc(16, sizeof(char));
 	int i;
+	//Prepare to store a reference to each room that this one is connected to
 	for(i = 0; i < 6; i ++){
 		room->outbound[i] = malloc(sizeof(struct Room));
 	}
@@ -44,10 +46,10 @@ void initRoom(struct Room* room){
 
 //Takes a Dungeon struct and initializes it to the default values of 7 capacity, 0 room count, and no name values
 void initDungeon(struct Dungeon* d){
-	d->capacity = 7;
-	d->numRooms = 0;
+	d->capacity = 7; //Provides the limit for how many rooms can be added to a dungeon
+	d->numRooms = 0; //Prepare to count how many rooms have been added to this dungeon
 	int i;
-	for(i = 0; i < d->capacity; i++){
+	for(i = 0; i < d->capacity; i++){ //Prepares each element of the dungeon map to store a room struct
         d->dungeonMap[i] = malloc(sizeof(struct Room));
 		initRoom(d->dungeonMap[i]);
 	}
@@ -59,7 +61,7 @@ void printDungeon(struct Dungeon* d){
 	printf("Room capacity: %d\n", d->capacity);
 	printf("Number of rooms assigned: %d\n", d->numRooms);
 	int i;
-	for(i = 0; i < d->capacity; i++){
+	for(i = 0; i < d->capacity; i++){ //Loops through the dungeon map and prints the name of each room stored therin
 		printf("Room %d is at: %p\n", i, d->dungeonMap[i]);
 		if(d->dungeonMap[i] != NULL){
 			printf("Name: %s\n", d->dungeonMap[i]->rmName);
@@ -72,15 +74,15 @@ int containsRoom(struct Dungeon* d, int id){
     int i;
     for(i = 0; i < d->capacity; i++){
         if(d->dungeonMap[i]->id == id){
-            return 1;
+            return 1; //Return 1 if the dungeon already contains a rooms with the given id
         }
     }
-    return 0;
+    return 0; //Return 0 if a room with an id matching the one given was not found in the dungeon map 
 }
 
 //Takes a room id and returns the corresponding name
 void getName(struct Dungeon* d, int id){
-    switch (id)
+    switch (id) //Finds the room name that corresponds to the given id and then assigns both the most recently added room in the dungeon map
     {
         case 1:
             d->dungeonMap[d->numRooms]->id = id;
@@ -96,7 +98,7 @@ void getName(struct Dungeon* d, int id){
             break;
         case 4:
             d->dungeonMap[d->numRooms]->id = id;
-            strcpy(d->dungeonMap[d->numRooms]->rmName, "Colosseum");
+            strcpy(d->dungeonMap[d->numRooms]->rmName, "Jail");
             break;
         case 5:
             d->dungeonMap[d->numRooms]->id = id;
@@ -108,7 +110,7 @@ void getName(struct Dungeon* d, int id){
             break;
         case 7:
             d->dungeonMap[d->numRooms]->id = id;
-            strcpy(d->dungeonMap[d->numRooms]->rmName, "Battlement");
+            strcpy(d->dungeonMap[d->numRooms]->rmName, "Armory");
             break;
         case 8:
             d->dungeonMap[d->numRooms]->id = id;
@@ -127,7 +129,7 @@ void getName(struct Dungeon* d, int id){
 
 //Used as the map is being generated to determine what a rooms type will be based on when it is added to the dungeon
 void getType(struct Dungeon* d){
-	switch(d->numRooms)
+	switch(d->numRooms) //Assigns type of START_ROOM to first room in dugeon map, END_ROOM to the last room, and MID_ROOM to all others
 	{
 		case 0:
 			strcpy(d->dungeonMap[d->numRooms]->type, "START_ROOM");
@@ -141,35 +143,35 @@ void getType(struct Dungeon* d){
 	}
 }
 
-//
+//Randomly selects 7 rooms to be used in the new dungeon
 void chooseRooms(struct Dungeon* d){
-    while(d->numRooms < d->capacity){
+    while(d->numRooms < d->capacity){ //While the number of rooms in the dungeon map is less than capacity of that dungeon, keep adding rooms
         int id = (rand() % 10) + 1;
-        if(containsRoom(d, id) != 1){
-            getName(d, id);
-	    getType(d);
-            d->numRooms++;
+        if(containsRoom(d, id) != 1){ //Only add room if the dungeon map does not already contain a room with the current id
+            getName(d, id); //Name newest room in map
+	    getType(d); //Assign type to newest room in map
+            d->numRooms++; //Increase count to indicate number of rooms currently in map
         }
     }
 }
 
-//Determines whether or not all files have been generated
+//Determines if each room in dungeon map has at least three connections 
 int fullyConnected(struct Dungeon* d){
 	int i;
 	for(i = 0; i < d->capacity; i++){
 		if(d->dungeonMap[i]->numConnect < 3){
-			return 0;
+			return 0; //returns 0 as soons as a room is found that doesn't have at least 3 connections
 		}
 	}
-	return 1;
+	return 1; //returns 1 if all rooms in dungeon map have at least 3 connections
 }
 
 //Determines if a connection can be added to the given room
 int hasRoom(struct Room* r){
 	if(r->numConnect < 6){
-		return 1;
+		return 1; //Returns 1 if the given room has rooms for another connection
 	}
-	return 0;
+	return 0; //Returns 0 if the given room cannot be connected to any more rooms
 }
 
 //Determines if the room corresponding to x's id has already been added to y's room connections
@@ -177,44 +179,44 @@ int alreadyAdded(struct Dungeon* d, int x, int y){
 	int i;
 	for(i = 0; i < d->dungeonMap[x]->numConnect; i++){
 		if(d->dungeonMap[y]->id == d->dungeonMap[x]->outbound[i]->id){
-			return 1;
+			return 1; //returns 1 if the rooms at index x of dungeon map is already connected to the room at index y
 		}
 	}	
-	return 0;
+	return 0; //returns 0 if rooms at index x and y of dungeon map have not already been connected
 }
 
 //Takes two rooms and adds each to the others connection list
 void connectRooms(struct Room* r1, struct Room* r2){
-	r1->outbound[r1->numConnect] = r2;
+	r1->outbound[r1->numConnect] = r2; //Adds room 2 to the connection list of room 1 and increments room 1's connection number
 	r1->numConnect++;
-	r2->outbound[r2->numConnect] = r1;
+	r2->outbound[r2->numConnect] = r1; //Adds room 1 to the connection list of room 2 and increments room 2's connection number
 	r2->numConnect++;	
 }
 
 //Determines which rooms can be connected togeter until all of the rooms have been sufficiently connected
 void connectDungeon(struct Dungeon* d){
-	while(fullyConnected(d) != true){
+	while(fullyConnected(d) != true){ //While there are rooms in dungeon map with fewer than 3 connections continue to connect rooms at random
 		int x = rand() % d->capacity;	
 		int y = rand() % d->capacity;
-		while(hasRoom(d->dungeonMap[x]) == false){
+		while(hasRoom(d->dungeonMap[x]) == false){ //Choose rooms at random until one is found that has room for more connections
 			x = rand() % d->capacity; 	
 		}
-		do{
+		do{ //Choose rooms at random until one is found that can be connected to the one at index x
 			y = rand() % d->capacity;
 		} while(x == y || hasRoom(d->dungeonMap[y]) == false || alreadyAdded(d, x, y) == true);
 		
-		connectRooms(d->dungeonMap[x], d->dungeonMap[y]);	
+		connectRooms(d->dungeonMap[x], d->dungeonMap[y]); //Connect the selected rooms to each other 
 	}
 }
 
 //Prints details for all of the rooms in the given dungeon. Only used for testing
 void printRooms(struct Dungeon* d){
    int i;
-   for(i = 0; i < 7; i++){
+   for(i = 0; i < 7; i++){ //Print the name of each room in dungeon map and the names of each room it is connected to
    	printf("%s is a %s and is connected to:\n", d->dungeonMap[i]->type, d->dungeonMap[i]->rmName); 
        	int j;
        	for(j = 0; j < 6; j++){
-            printf("%s ", d->dungeonMap[i]->outbound[j]->rmName);
+            printf("%s ", d->dungeonMap[i]->outbound[j]->rmName); //Print the name of each room that this one is connected to
        
         }
         printf("\n");
@@ -223,49 +225,49 @@ void printRooms(struct Dungeon* d){
 
 //Creats a new directory and returns its name
 char* newDir(){
-    int procid = getpid();
+    int procid = getpid(); 
     char dirPrfx [32] = "shandst.rooms.";
     char* dirName = calloc(32, sizeof(char));
-    sprintf(dirName, "%s%d", dirPrfx, procid);
-    mkdir(dirName, 0776);
+    sprintf(dirName, "%s%d", dirPrfx, procid); //Smash the directory prefix and current process id together into a unique directory name
+    mkdir(dirName, 0776); //Make uniquely named directory
 
     return dirName;
 }
 
 int main(){
 
-    	srand(time(0));
+    	srand(time(0)); //Seed random number generation for selecting random rooms and their connections
 
-	struct Dungeon* dungeon = malloc(sizeof(struct Dungeon));
-	initDungeon(dungeon);
-    	chooseRooms(dungeon);
-	connectDungeon(dungeon);
+	struct Dungeon* dungeon = malloc(sizeof(struct Dungeon)); //Reserve memory on the heap for a dungeon struct
+	initDungeon(dungeon); //Initialize the dungeon to default values
+    	chooseRooms(dungeon); //Randomly select 7 rooms and add them to the dungeon
+	connectDungeon(dungeon); //Randomly connect each room in the dungeon to at least 3 others but no more than 6
 
 	char* curDir = newDir();
 	int i;
-	for(i = 0; i < dungeon->numRooms; i++){
-		char path[32] = {'\0'};
-		sprintf(path, "%s/%s", curDir, dungeon->dungeonMap[i]->rmName);
-		int fd = open(path, O_WRONLY | O_APPEND | O_CREAT, 0644);
-		if(fd == -1){
+	for(i = 0; i < dungeon->numRooms; i++){ //Increment through each room in the dungeon and write its details to the corresponding file in the newly created shandst.rooms.<PID> folder
+		char path[32] = {'\0'}; //Prepare to store the path to the soon to be created room file
+		sprintf(path, "%s/%s", curDir, dungeon->dungeonMap[i]->rmName); //Build and store the path to be used for creating the current rooms new file
+		int fd = open(path, O_WRONLY | O_APPEND | O_CREAT, 0644); //Create new file room
+		if(fd == -1){ //Report failure to create room file and exit
 			printf("Failed to open file\n");
 			perror("While opening file");
 			exit(1);
 		}
 	
 		char* output = calloc(32, sizeof(char));
-		memset(output, '\0', 32);
-		sprintf(output, "ROOM NAME: %s\n", dungeon->dungeonMap[i]->rmName);
-		write(fd, output, strlen(output) * sizeof(char));
+		memset(output, '\0', 32); //Prepare string for each use
+		sprintf(output, "ROOM NAME: %s\n", dungeon->dungeonMap[i]->rmName); //Prepare the string 'ROOM NAME: <current room>\n' to be written to the start of the current file
+		write(fd, output, strlen(output) * sizeof(char)); //Write room name to file
 		int j;
-		for(j = 0; j < dungeon->dungeonMap[i]->numConnect; j++){
-			memset(output, '\0', 32);
+		for(j = 0; j < dungeon->dungeonMap[i]->numConnect; j++){ //Prepare and append strings of format 'CONNECTION <number>: <room name>\n' to current file
+			memset(output, '\0', 32); //Prepare string for each use
 			sprintf(output, "CONNECTION %d: %s\n", j + 1, dungeon->dungeonMap[i]->outbound[j]->rmName);
-			write(fd, output, strlen(output) * sizeof(char));
+			write(fd, output, strlen(output) * sizeof(char)); //Writes each of a rooms connections to its own line in the file
 		}
-		memset(output, '\0', 32);
-		sprintf(output, "ROOM TYPE: %s\n", dungeon->dungeonMap[i]->type);
-		write(fd, output, strlen(output) * sizeof(char));
+		memset(output, '\0', 32); //Prepare string for re-use
+		sprintf(output, "ROOM TYPE: %s\n", dungeon->dungeonMap[i]->type); //Prepare the string 'ROOM TYPE: <type>\n' to be appended to the end of the file
+		write(fd, output, strlen(output) * sizeof(char)); //Appends current rooms type to the last line of the current room file
 	}
  
 	return 0;
